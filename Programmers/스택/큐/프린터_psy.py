70aedd285bc507b373a4bf29257e07823993adc6
Soyeon Park <paksuua@naver.com>
2021-07-21 22:45:51 +0900

    ADD 프로그래머스 프린터.py

--- Programmers/스택/큐/프린터_psy.py
+++ Programmers/스택/큐/프린터_psy.py
@@ -1,18 +1,34 @@
 import collections
 
 def solution(priorities, location):
-    first = max(priorities)
-    # print("max", first)
     priorities = collections.deque(priorities)
-    
-    while(first != priorities[0]):
-        # print(priorities)
-        priorities.rotate(-1)
-        
-        if location>0:
-            location-=1
+    count=0
+
+    while(priorities):
+
+        # 가장 큰 수
+        pNum = max(priorities)
+
+        # 현재 인덱스의 수가 가장 큰 수라면
+        if pNum == priorities[0]:
+            count+=1
+            
+            # 찾으려는 인덱스를 pop
+            if location == 0:
+                # print("answer", count) 
+                return count
+ 
+            priorities.popleft()
+
+        # 가장 큰 수가 맨 앞에 올 때 까지 큐 회전
         else:
-            location=len(priorities)-1
+            priorities.rotate(-1)
         
-    # print("location: ", location)
-    return location+1
+        location -=1
+        if (location<0):
+            location += len(priorities)
+
+
+# solution([2, 1, 3, 2], 2) # 1
+# solution([1, 1, 9, 1, 1, 1], 0) # 5
+# solution([2, 1, 9, 1, 9, 1], 1) # 4
