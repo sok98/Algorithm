1919b4837bce3730f9aff2a3386e250aa0dcb914
SeoYeseul <sok98@naver.com>
2021-07-29 08:29:19 +0900

    ADD 백준  1463 1로 만들기

---
+++ BOJ/7월/0728(수)/1463_sys.py
@@ -0,0 +1,26 @@
+import sys
+from collections import defaultdict
+
+n = int(sys.stdin.readline())
+count = defaultdict(lambda : 0)
+
+def calcul(num, n):
+    if count[num] == 0 or count[num] > count[n] + 1:
+        count[num] = count[n] + 1
+
+        if num%3 == 0:
+            calcul(num//3, num)
+        if num%2 == 0:
+            calcul(num//2, num)
+        if num > 1:
+            calcul(num-1, num)
+        
+
+if n%3 == 0:
+    calcul(n//3, n)
+if n%2 == 0:
+    calcul(n//2, n)
+if n > 1:
+    calcul(n-1, n)
+
+print(count[1])
