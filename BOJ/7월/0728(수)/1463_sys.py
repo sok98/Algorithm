8b0b0ec2632f6bf3d57560f4b3e35f5ea34e444f
SeoYeseul <sok98@naver.com>
2021-07-29 09:14:33 +0900

    UPDATE 1463 1로 만들기

--- BOJ/7월/0728(수)/1463_sys.py
+++ BOJ/7월/0728(수)/1463_sys.py
@@ -1,26 +1,18 @@
 import sys
 from collections import defaultdict
 
-n = int(sys.stdin.readline())
-count = defaultdict(lambda : 0)
+number = int(sys.stdin.readline())
+count = defaultdict(lambda : 999999)
+count[1], count[2], count[3] = 0, 1, 1
 
-def calcul(num, n):
-    if count[num] == 0 or count[num] > count[n] + 1:
-        count[num] = count[n] + 1
+for n in range(4, number+1):
+    if n%3 == 0 and n%2 == 0:
+        count[n] = min(count[n//3], count[n//2], count[n-1]) + 1
+    elif n%3 == 0:
+        count[n] = min(count[n//3], count[n-1]) + 1
+    elif n%2 == 0:
+        count[n] = min(count[n//2], count[n-1]) + 1
+    else:
+        count[n] = count[n-1] + 1
 
-        if num%3 == 0:
-            calcul(num//3, num)
-        if num%2 == 0:
-            calcul(num//2, num)
-        elif num > 1:
-            calcul(num-1, num)
-        
-
-if n%3 == 0:
-    calcul(n//3, n)
-if n%2 == 0:
-    calcul(n//2, n)
-elif n > 1:
-    calcul(n-1, n)
-
-print(count[1])
+print(count[number])
