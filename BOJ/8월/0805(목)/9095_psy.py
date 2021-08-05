926ebda2f5e8b56cefff68f65c5b97b7ddba447a
Soyeon Park <paksuua@naver.com>
2021-08-06 00:25:51 +0900

    ADD 백준 9095 1, 2, 3 더하기

--- BOJ/8월/0805(목)/9095_psy.py
+++ BOJ/8월/0805(목)/9095_psy.py
@@ -1,7 +1,7 @@
 # [silver-3] 9095 1, 2, 3 더하기
-# algorithm 
-# 메모리: KB
-# 시간: ms
+# algorithm 다이나믹 프로그래밍
+# 메모리: 	29200KB
+# 시간: 76ms
 
 T = int(input())
 
@@ -10,7 +10,9 @@ result = []
 
 for _ in range(T):
     n = int(input())
-
-    for i in range(4, n+1):
+    for i in range(len(nList), n+1):
         nList.append(nList[i-1] + nList[i-2] + nList[i-3])
-    result(nList[n])
+    result.append(nList[n])
+
+for r in result:
+    print(r)
