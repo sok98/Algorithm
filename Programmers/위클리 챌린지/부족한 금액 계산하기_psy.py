b5f2ad28dc74825d2e9d0301e9d209b8b902c455
Soyeon Park <paksuua@naver.com>
2021-08-06 09:37:57 +0900

    ADD 프로그래머스 부족한 금액 계산하기.py

--- Programmers/위클리 챌린지/부족한 금액 계산하기_psy.py
+++ Programmers/위클리 챌린지/부족한 금액 계산하기_psy.py
@@ -1,2 +1,14 @@
 # [Weekly Chanllenge] 부족한 금액 계산하기
-# 획득 포인트
+# 획득 포인트 4
+
+def solution(price, money, count):
+    sum = 0
+    
+    for i in range(1, count+1):
+        sum+=(i*price)
+    answer = sum - money
+    
+    if answer < 0:
+        answer = 0
+    
+    return answer
