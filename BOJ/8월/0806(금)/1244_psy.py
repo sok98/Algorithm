8ee3329a33d30aabb81e88c4fee093977a5bef9c
Soyeon Park <paksuua@naver.com>
2021-08-07 22:01:51 +0900

    UPDATE 백준 1244 스위치 켜고 끄기

--- BOJ/8월/0806(금)/1244_psy.py
+++ BOJ/8월/0806(금)/1244_psy.py
@@ -1,7 +1,9 @@
 # [silver-4] 1244 스위치 켜고 끄기
 # algorithm 구현, 시뮬레이션
-# 메모리: 	KB
-# 시간:     ms
+# 메모리: 	29200 KB
+# 시간:     80 ms
+
+# 0 1 0 1 0 0 0 1
 
 import sys
 input = sys.stdin.readline
@@ -9,46 +11,33 @@ input = sys.stdin.readline
 N = int(input())
 
 switches = [-1]
-student = {}
-switches += (list(input().split()))
+change = {0:1, 1:0}
+switches += (list(map(int, input().split())))
     
 S = int(input())
-for i in range(1, S+1):
-    s, c = map(int, input().split())
-    student[s] = c
+for _ in range(S):
+    gender, index = map(int, input().split())
 
-# 남학생 스위치
-for i in range(student[1], N+1, student[1]):
-    if switches[i]=='0':
-        switches[i] = '1'
-    else:
-        switches[i] = '0'
+    # 남학생 스위치
+    if gender == 1:
+        for i in range(index, len(switches), index):
+            switches[i] = change[switches[i]]
 
-result = ' '.join(switches[1:])
-print(result)
+    # 여학생 스위치
+    elif gender == 2:
+        for i in range(0, len(switches)):
+            if i == 0:
+                switches[index] = change[switches[index]]
+                continue
 
-# 여학생 스위치
-for i in range(N+1):
-    if i ==0:
-        if switches[student[2]-i]=='0':
-            switches[student[2]-i] = '1'
-        else:
-            switches[student[2]-i] = '0'
-        continue
+            if index-i<1 or len(switches)<=index+i:
+                break
 
-    if student[2]-i<1:
-        break
+            if switches[index-i] == switches[index+i]:
+                switches[index-i] = change[switches[index-i]]
+                switches[index+i] = change[switches[index+i]]
+            else:
+                break
 
-    if switches[student[2]-i] == switches[student[2]+i]:
-        if switches[student[2]-i]=='0':
-            switches[student[2]-i] = '1'
-        else:
-            switches[student[2]-i] = '0'
-
-        if switches[student[2]+i]=='0':
-            switches[student[2]+i] = '1'
-        else:
-            switches[student[2]+i] = '0'
-
-result = ' '.join(switches[1:])
-print(result)
+for i in range(1, len(switches), 20):
+    print(*switches[i:i+20]) # print(* => 리스트 아이템을 공백 문자로 구분지어 출력
