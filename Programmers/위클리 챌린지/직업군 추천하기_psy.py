722878a54d8b6013b9a906456d1192f7a2640091
Soyeon Park <paksuua@naver.com>
2021-08-24 20:52:01 +0900

    ADD 프로그래머스 위클리챌린지 4주차 직업군 추천하기

---
+++ Programmers/위클리 챌린지/직업군 추천하기_psy.py
@@ -0,0 +1,29 @@
+# [Weekly Chanllenge] 직업군 추천하기
+# 획득 포인트 4
+
+from collections import defaultdict
+
+def solution(table, languages, preference):
+    answer = ''
+    skills = {}
+    groups = defaultdict(int)
+    
+    for t in table:
+        temp = list(map(str, t.split()))
+        tl = temp[1:]
+        tl.reverse()
+        skills[temp[0]] = tl
+        
+        for i in range(len(languages)):
+            if languages[i] in tl:
+                groups[temp[0]] += (tl.index(languages[i])+1)*preference[i]
+    max = 0
+    gLan =[]
+    for key, value in groups.items():
+        if value > max:
+            gLan = [key]
+            max = value
+        elif value == max:
+            gLan.append(key)
+            
+    return sorted(gLan)[0]
