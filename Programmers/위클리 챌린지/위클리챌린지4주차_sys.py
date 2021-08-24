2ff1bf782c3582240ea2d135666a2a90ee34fb36
SeoYeseul <sok98@naver.com>
2021-08-24 21:07:58 +0900

    Refactor 위클리 챌린지 4주차

--- Programmers/위클리 챌린지/위클리챌린지4주차_sys.py
+++ Programmers/위클리 챌린지/위클리챌린지4주차_sys.py
@@ -1,6 +1,5 @@
 def solution(table, languages, preference):
     answer = []
-    # new_table = [list(t.split()) for t in table]
     new_table = sorted([list(t.split()) for t in table], key=lambda x : x[0])
     job = {n: new_table[n][0] for n in range(len(new_table))}
 
@@ -12,9 +11,3 @@ def solution(table, languages, preference):
         answer.append(total)
 
     return job[answer.index(max(answer))]
-
-
-# print(solution(["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"],
-# ["PYTHON", "C++", "SQL"], [7, 5, 5]))
-print(solution(["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"],
-["JAVA", "JAVASCRIPT"], [7, 5]))
