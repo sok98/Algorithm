a5727b7c750dacb0daefbbb71577b253b91cb7bc
Soyeon Park <paksuua@naver.com>
2021-09-02 08:56:36 +0900

    UPDATE 백준 1753 최단경로

--- BOJ/9월/0901(수)/1753_psy.py
+++ BOJ/9월/0901(수)/1753_psy.py
@@ -49,7 +49,7 @@ dijkstra(start)
 for i in range(1, n + 1):
     # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
     if distance[i] == INF:
-        print("INFINITY")
+        print("INF")
     # 도달할 수 있는 경우 거리를 출력
     else:
         print(distance[i])
