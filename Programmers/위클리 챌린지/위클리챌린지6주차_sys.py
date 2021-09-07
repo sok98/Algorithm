88168e2e4f73ccdca2ffeb39107215964df231cc
SeoYeseul <sok98@naver.com>
2021-09-08 08:40:09 +0900

    Refactor: 주석 수정, 파일 이동

--- Programmers/위클리 챌린지/위클리챌린지6주차_sys.py
+++ Programmers/위클리 챌린지/위클리챌린지6주차_sys.py
@@ -1,11 +1,11 @@
 def solution(weights, head2head):
     # 선수번호 : [승리 횟수(나중에 승률로 바뀜), 무거운 사람 승리 횟수, 자기 몸무게]
-    player = { i+1:[0.0, 0, weights[i]] for i in range(len(weights)) }   
+    player = { i+1:[0, 0, weights[i]] for i in range(len(weights)) }   
 
     for idx, score in enumerate(head2head):
         cnt = 0
         for i, s in enumerate(score):
-            if s != "N": cnt += 1   # 대결 횟수 -1
+            if s != "N": cnt += 1   # 대결 횟수 +1
             if s == "W":
                 player[idx+1][0] += 1   # 승리 횟수 +1
                 if weights[idx] < weights[i]:
